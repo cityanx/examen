091063d783bb4a485385559961f7b1177ec390fb
cityanx <andres12enamorado@gmail.com>
2021-06-18 08:03:46 -0600

    examen

---
+++ DatosPaciente/DatosPaciente/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DatosPaciente.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
